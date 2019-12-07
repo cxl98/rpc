0df0f2800466647677a7bb3bbd627e4d2907f8d1
cxl <1799359457@qq.com>
2019-12-07 21:12:03 +0800

    update

--- rpc-core/src/main/java/com/cxl/rpc/remoting/net/impl/netty/server/NettyServerHandler.java
+++ rpc-core/src/main/java/com/cxl/rpc/remoting/net/impl/netty/server/NettyServerHandler.java
@@ -1,9 +1,13 @@
 package com.cxl.rpc.remoting.net.impl.netty.server;
 
+import com.cxl.rpc.remoting.net.params.Beat;
 import com.cxl.rpc.remoting.net.params.RpcRequest;
+import com.cxl.rpc.remoting.net.params.RpcResponse;
 import com.cxl.rpc.remoting.provider.RpcProviderFactory;
+import com.cxl.rpc.util.ThrowableUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.timeout.IdleStateEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,6 +26,44 @@ public class NettyServerHandler extends SimpleChannelInboundHandler<RpcRequest> 
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, RpcRequest msg) throws Exception {
+        //filter beat
+        if (Beat.BEAT_ID.equals(msg.getRequestId())) {
+            LOGGER.debug(">>>>>>>>>>>>>>>>>>rpc provider netty server read beat-ping");
+            return;
+        }
+        //do invoke
+        try {
+            serverHandlerPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    //invoke +response
+                    RpcResponse response=rpcProviderFactory.invokeService(msg);
+                    ctx.writeAndFlush(response);
+                }
+            });
+        } catch (Exception e) {
+            //catch error
+            RpcResponse response=new RpcResponse();
+            response.setRequestId(msg.getRequestId());
+            response.setErrorMsg(ThrowableUtil.toString(e));
+            ctx.writeAndFlush(response);
 
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        LOGGER.error(">>>>>>>>>>>>> rpc provider netty server caught exception.",cause);
+        ctx.close();
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent){
+            ctx.channel().close();
+            LOGGER.debug(">>>>>>>>>>>>>>>>>>>>rpc provider netty server close an odle channel.");
+        }else{
+            super.userEventTriggered(ctx,evt);
+        }
     }
 }
