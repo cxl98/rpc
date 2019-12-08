f1e8c1bd7540cac315281b03955d9b7a3f8b9fcd
cxl <1799359457@qq.com>
2019-12-08 22:56:23 +0800

    update

--- rpc-core/src/main/java/com/cxl/rpc/remoting/invoker/call/RpcInvokeFuture.java
+++ rpc-core/src/main/java/com/cxl/rpc/remoting/invoker/call/RpcInvokeFuture.java
@@ -1,33 +1,84 @@
 package com.cxl.rpc.remoting.invoker.call;
 
+import com.cxl.rpc.remoting.net.params.RpcFutureResponse;
+import com.cxl.rpc.remoting.net.params.RpcResponse;
+import com.cxl.rpc.util.RpcException;
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 public class RpcInvokeFuture implements Future {
+    private RpcFutureResponse futureResponse;
+
+    public RpcInvokeFuture(RpcFutureResponse futureResponse) {
+        this.futureResponse = futureResponse;
+    }
+
+    public void stop(){
+        //remove-InvokerFuture
+        futureResponse.removeInvokerFuture();
+    }
+
     @Override
     public boolean cancel(boolean mayInterruptIfRunning) {
-        return false;
+        return futureResponse.cancel(mayInterruptIfRunning);
     }
 
     @Override
     public boolean isCancelled() {
-        return false;
+        return futureResponse.isCancelled();
     }
 
     @Override
     public boolean isDone() {
-        return false;
+        return futureResponse.isDone();
     }
 
     @Override
     public Object get() throws InterruptedException, ExecutionException {
-        return null;
+        try {
+            return get(-1,TimeUnit.MILLISECONDS);
+        } catch (TimeoutException e) {
+            throw new RpcException(e);
+        }
     }
 
     @Override
     public Object get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-        return null;
+        try {
+            //future get
+            RpcResponse response=futureResponse.get(timeout,unit);
+
+            if (response.getErrorMsg() != null) {
+                throw new RpcException(response.getErrorMsg());
+            }
+            return response.getResult();
+        } finally {
+            stop();
+        }
+    }
+
+    private static ThreadLocal<RpcInvokeFuture> threadInvokerFuture=new ThreadLocal<>();
+
+    /**
+     * get future
+     * @param type
+     * @param <T>
+     * @return
+     */
+    public static <T> Future<T> getFuture(Class<T> type){
+        Future<T> future=(Future<T>) threadInvokerFuture.get();
+        threadInvokerFuture.remove();
+        return future;
+    }
+
+    public static void setFuture(RpcInvokeFuture future){
+        threadInvokerFuture.set(future);
+    }
+
+    public static void removeFuture(){
+        threadInvokerFuture.remove();
     }
 }
