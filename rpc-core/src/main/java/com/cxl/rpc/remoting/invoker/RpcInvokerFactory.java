7ede46c90e299e5064effae18c900ab8bdef96ea
cxl <1799359457@qq.com>
2020-03-15 10:54:53 +0800

    update

--- rpc-core/src/main/java/com/cxl/rpc/remoting/invoker/RpcInvokerFactory.java
+++ rpc-core/src/main/java/com/cxl/rpc/remoting/invoker/RpcInvokerFactory.java
@@ -5,14 +5,14 @@ import com.cxl.rpc.registry.impl.LocalRegistry;
 import com.cxl.rpc.remoting.net.params.BaseCallback;
 import com.cxl.rpc.remoting.net.params.RpcFutureResponse;
 import com.cxl.rpc.remoting.net.params.RpcResponse;
+import com.cxl.rpc.util.RpcException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.*;
 
 public class RpcInvokerFactory {
 
@@ -40,13 +40,6 @@ public class RpcInvokerFactory {
         this.serviceRegistryClass = serviceRegistryClass;
         this.serviceRegistryParam = serviceRegistryParam;
     }
-    //---------------------service registry-------------------
-    private ServiceRegistry serviceRegistry;
-    public ServiceRegistry getServiceRegistry(){return serviceRegistry;}
-
-    private List<BaseCallback> stopCallbackList=new ArrayList<>();
-
-
     //---------------------start / stop--------------------
 
     public void start()throws Exception{
@@ -79,16 +72,19 @@ public class RpcInvokerFactory {
         stopCallbackThreadPool();
     }
 
+    //---------------------service registry-------------------
+    private ServiceRegistry serviceRegistry;
+    public ServiceRegistry getServiceRegistry(){return serviceRegistry;}
+
+    private List<BaseCallback> stopCallbackList=new ArrayList<>();
+
     public void addStopCallback(BaseCallback callback){
         stopCallbackList.add(callback);
     }
-    private void stopCallbackThreadPool() {
-
-    }
 
     //------------------------future-response pool----------------------------
-
     private ConcurrentMap<String, RpcFutureResponse> futureResponsePool=new ConcurrentHashMap<>();
+
     public void setInvokerFuture(String requestId,RpcFutureResponse futureResponse){
         futureResponsePool.put(requestId
         ,futureResponse);
@@ -105,7 +101,58 @@ public class RpcInvokerFactory {
 
         //notify
         if (futureResponse.getInvokeCallback()!=null) {
+            //回调类型
+            try {
+                executeResponseCallback(new Runnable(){
 
+                    @Override
+                    public void run() {
+                       if (null!=rpcResponse.getErrorMsg()){
+                           futureResponse.getInvokeCallback().onFailure(new RpcException(rpcResponse.getErrorMsg()));
+                       }else{
+                           futureResponse.getInvokeCallback().onSuccess(rpcResponse.getResult());
+                       }
+                    }
+                });
+            } catch (Exception e) {
+                LOGGER.error(e.getMessage(),e);
+            }
+        }else{
+            // 其他回调类型
+            futureResponse.setResponse(rpcResponse);
+        }
+        //删除该实例
+        futureResponsePool.remove(requestId);
+    }
+    //------------------response callback ThreadPool-------------------
+    private ThreadPoolExecutor threadPoolExecutor=null;
+    private void executeResponseCallback(Runnable runnable) {
+        if (null == threadPoolExecutor) {
+            synchronized (this) {
+                if (null == threadPoolExecutor) {
+                    threadPoolExecutor = new ThreadPoolExecutor(20, 100, 60L, TimeUnit.SECONDS, new LinkedBlockingDeque<>(1000), new ThreadFactory() {
+                        @Override
+                        public Thread newThread(Runnable r) {
+                            return new Thread(r, "rpc, RpcInvokerFactory-responseCallbackThreadPool-" + r.hashCode());
+                        }
+                    }, new RejectedExecutionHandler() {
+                        @Override
+                        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+                            throw new RpcException("rpc Invoke Callback Thread pool is EXHAUSTED!");
+                        }
+                    });
+
+                }
+            }
+        }
+        threadPoolExecutor.execute(runnable);
+    }
+
+
+
+    private void stopCallbackThreadPool() {
+        if (null!=threadPoolExecutor){
+            threadPoolExecutor.shutdown();
         }
     }
 }
