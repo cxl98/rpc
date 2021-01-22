441538a79a63e0e39ea1a384c8c2eabffd98d39b
cxl <1799359457@qq.com>
2021-01-22 20:12:42 +0800

    修改bug

--- rpc-core/src/main/java/com/cxl/rpc/remoting/consumer/route/LoadBalance.java
+++ rpc-core/src/main/java/com/cxl/rpc/remoting/consumer/route/LoadBalance.java
@@ -5,10 +5,25 @@ import com.cxl.rpc.remoting.consumer.route.impl.*;
 import java.util.TreeSet;
 
 public enum  LoadBalance {
+    /**
+     * 随机算法
+     */
     RANDOM(new RpcLoadBalanceRandomStrategy()),
+    /**
+     *轮询算法
+     */
     ROUND(new RpcLoadBalanceRoundStrategy()),
+    /**
+     * LRU算法
+     */
     LRU(new RpcLoadBalanceLRUStrategy()),
+    /**
+     * LFU算法
+     */
     LFU(new RpcLoadBalanceLFUStrategy()),
+    /**
+     * 一致性hash算法
+     */
     CONSISTENT_HASH(new RpcLoadBalanceConsistentHashStrategy());
 
     public final RpcLoadBalance rpcLoadBalance;
@@ -26,27 +41,27 @@ public enum  LoadBalance {
         return defaultRouter;
     }
 
-     public static void main(String[] args) {
-        String serviceKey = "service";
-        TreeSet<String> addressSet = new TreeSet<String>(){{
-            add("1");
-            add("2");
-            add("3");
-            add("4");
-            add("5");
-        }};
-
-//        for (LoadBalance item : LoadBalance.values()) {
-//            long start = System.currentTimeMillis();
-//            for (int i = 0; i < 10000; i++) {
-//                String address =item.rpcLoadBalance.route(serviceKey,addressSet);
-//                System.out.println(address);
-//            }
-//            long end = System.currentTimeMillis();
-//            System.out.println(item.name() + " --- " + (end-start));
-//        }
-         LoadBalance lru = LoadBalance.match("LRU", LoadBalance.ROUND);
-         System.out.println(lru.name());
-
-     }
+//     public static void main(String[] args) {
+//        String serviceKey = "service";
+//        TreeSet<String> addressSet = new TreeSet<String>(){{
+//            add("1");
+//            add("2");
+//            add("3");
+//            add("4");
+//            add("5");
+//        }};
+//
+////        for (LoadBalance item : LoadBalance.values()) {
+////            long start = System.currentTimeMillis();
+////            for (int i = 0; i < 10000; i++) {
+////                String address =item.rpcLoadBalance.route(serviceKey,addressSet);
+////                System.out.println(address);
+////            }
+////            long end = System.currentTimeMillis();
+////            System.out.println(item.name() + " --- " + (end-start));
+////        }
+//         LoadBalance lru = LoadBalance.match("LRU", LoadBalance.ROUND);
+//         System.out.println(lru.name());
+//
+//     }
 }
