c819e336baeda366a64716447dd778ff988c692c
cxl <1799359457@qq.com>
2021-02-01 15:04:29 +0800

    修改bug

--- rpc-core/src/main/java/com/cxl/rpc/remoting/consumer/route/impl/RpcLoadBalanceRoundStrategy.java
+++ rpc-core/src/main/java/com/cxl/rpc/remoting/consumer/route/impl/RpcLoadBalanceRoundStrategy.java
@@ -8,29 +8,32 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class RpcLoadBalanceRoundStrategy extends RpcLoadBalance {
-    private ConcurrentMap<String,Integer> concurrentMap=new ConcurrentHashMap<>();
-    private long CACHE_VALID_TIME=0;
+    private final ConcurrentMap<String, Integer> concurrentMap = new ConcurrentHashMap<>();
+    /**
+     * 缓存有效时间
+     */
+    private long cvt = 0;
 
-    private int count(String serviceKey){
+    private int count(String serviceKey) {
         //cache clear
-        if (System.currentTimeMillis()>CACHE_VALID_TIME){
+        if (System.currentTimeMillis() > cvt) {
             concurrentMap.clear();
-            CACHE_VALID_TIME=System.currentTimeMillis()+24*60*60*1000;
+            cvt = System.currentTimeMillis() + 24 * 60 * 60 * 1000;
         }
 
         //count++
-        Integer count=concurrentMap.get(serviceKey);
-        count=(count==null||count>1000000)?(new Random().nextInt(100)):++count;
-        concurrentMap.put(serviceKey,count);
+        Integer count = concurrentMap.get(serviceKey);
+        count = (count == null || count > 1000000) ? (new Random().nextInt(100)) : ++count;
+        concurrentMap.put(serviceKey, count);
         return count;
     }
+
     @Override
     public String route(String serviceKey, TreeSet<String> addressSet) {
         //arr
-        String[] addressArr=addressSet.toArray(new String[addressSet.size()]);
+        String[] addressArr = addressSet.toArray(new String[addressSet.size()]);
 
         //round
-        String finalAddress=addressArr[count(serviceKey)%addressArr.length];
-        return finalAddress;
+        return addressArr[count(serviceKey) % addressArr.length];
     }
 }
