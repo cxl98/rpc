4023625b71076a47d4fdcc34f33a979a6d6f144e
cxl <1799359457@qq.com>
2021-02-13 15:13:22 +0800

    修改bug

--- rpc-core/src/test/java/Client.java
+++ rpc-core/src/test/java/Client.java
@@ -4,12 +4,28 @@ import com.cxl.rpc.remoting.net.ConnectClient;
 import com.cxl.rpc.remoting.net.impl.netty.client.NettyClient;
 import com.cxl.rpc.remoting.net.params.RpcRequest;
 
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.UUID;
 
 public class Client {
     public static void main(String[] args) throws Exception {
-        RpcReferenceBean rpcReferenceBean=new RpcReferenceBean();
-        rpcReferenceBean.setAddress("127.0.0.1:8888");
-        rpcReferenceBean.setIface(API.class);
+//        RpcReferenceBean rpcReferenceBean=new RpcReferenceBean();
+//        rpcReferenceBean.setAddress("127.0.0.1:8888");
+//        rpcReferenceBean.setIface(API.class);
+
+        SortedMap<Integer, String> mp = new TreeMap<>();
+
+        // Adding Element to SortedSet
+        mp.put(1, "One");
+        mp.put(2, "Two");
+        mp.put(3, "Three");
+        mp.put(4, "Four");
+        mp.put(5, "Five");
+
+        // Returning the key greater
+        // than or equal to 2
+        System.out.print("Last Key in the map is : "
+                + mp.tailMap(2));
     }
 }
