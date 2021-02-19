a40aec651e8f4af3eebb36160a79caf78a3fe4eb
cxl <1799359457@qq.com>
2021-02-19 21:35:24 +0800

    修改bug

--- rpc-core/src/test/java/Iptest.java
+++ rpc-core/src/test/java/Iptest.java
@@ -2,10 +2,12 @@ import com.cxl.rpc.util.IpUtil;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.UUID;
 
 public class Iptest {
     public static void main(String[] args) throws UnknownHostException {
         System.out.println(InetAddress.getLocalHost());
         System.out.println(IpUtil.getLocalAddress());
+        System.out.println(UUID.randomUUID());
     }
 }
