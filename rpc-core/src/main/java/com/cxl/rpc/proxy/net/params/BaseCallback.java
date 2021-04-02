f6e34f57fc7d7957400227bd54414d542a2b7a66
cxl <1799359457@qq.com>
2021-04-02 22:56:36 +0800

    update

--- rpc-core/src/main/java/com/cxl/rpc/proxy/net/params/BaseCallback.java
+++ rpc-core/src/main/java/com/cxl/rpc/proxy/net/params/BaseCallback.java
@@ -1,5 +1,8 @@
 package com.cxl.rpc.proxy.net.params;
 
+/**
+ * @author cxl
+ */
 public abstract class BaseCallback {
     public abstract void run() throws Exception;
 }
--- rpc-core/src/main/java/com/cxl/rpc/proxy/net/params/Beat.java
+++ rpc-core/src/main/java/com/cxl/rpc/proxy/net/params/Beat.java
@@ -1,5 +1,8 @@
 package com.cxl.rpc.proxy.net.params;
 
+/**
+ * @author cxl
+ */
 public class Beat {
     public static final int BEAT_INTERVAL = 10;
     public static final String BEAT_ID="BEAT_PING_PONG";
